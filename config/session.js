fb536e9e5e7361be29c8c60dcd7e3eba72786a62
Nicolas M <mbark.nicolas@gmail.com>
2025-02-11 15:58:30 +0100

    fix

--- config/session.js
+++ config/session.js
@@ -3,15 +3,7 @@ const MongoStore = require("connect-mongo");
 module.exports.session = {
   secret: process.env.SESSION_SECRET || "d54dcb5dc070ec612015b0e93ba026f9",
 
-  // Sails utilise "mongo" au lieu de "connect-mongo" pour l'adaptateur
-  adapter: "mongo",
-  
-  // Connexion MongoDB
-  url: "mongodb://admin:pissenlit2025!@127.0.0.1:27017/sessions?authSource=admin",
-  collection: "sessions",
-  ttl: 14 * 24 * 60 * 60, // 14 jours
-
-  // Configuration du stockage des sessions
+  // Utilisation correcte de `store` pour éviter l'erreur
   store: MongoStore.create({
     mongoUrl: "mongodb://admin:pissenlit2025!@127.0.0.1:27017/sessions?authSource=admin",
     collectionName: "sessions",
@@ -24,4 +16,5 @@ module.exports.session = {
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 jour
   },
+  
 };
