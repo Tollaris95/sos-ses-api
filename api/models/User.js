e29420b809a6f6e2ab95b7d22624b0d14e1d41c1
Nicolas M <mbark.nicolas@gmail.com>
2025-02-11 15:48:36 +0100

    fix

--- api/models/User.js
+++ api/models/User.js
@@ -18,4 +18,5 @@ module.exports = {
   },
 
   primaryKey: "id",
+  datastore: "default",
 };
--- config/session.js
+++ config/session.js
@@ -1,18 +1,19 @@
 const MongoStore = require("connect-mongo");
 
 module.exports.session = {
-  secret: "d54dcb5dc070ec612015b0e93ba026f9",
+  secret: process.env.SESSION_SECRET || "d54dcb5dc070ec612015b0e93ba026f9",
 
+  adapter: "connect-mongo", // S'assurer que l'adaptateur est bien défini
   store: MongoStore.create({
     mongoUrl: "mongodb://admin:pissenlit2025!@127.0.0.1:27017/sessions?authSource=admin",
     collectionName: "sessions",
     ttl: 14 * 24 * 60 * 60, // 14 jours d'expiration
-    autoRemove: "native",  // Nettoyage automatique des sessions expirées
+    autoRemove: "native", // Nettoyage automatique des sessions expirées
   }),
 
   cookie: {
-    secure: process.env.NODE_ENV === "production", // En production, le cookie sera envoyé uniquement sur HTTPS
+    secure: process.env.NODE_ENV === "production", // Cookie sécurisé en prod
     httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000, // 1 jour
+    maxAge: 24 * 60 * 60 * 1000, // 1 jour d'expiration
   },
 };
