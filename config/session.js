b617a59d3a99156de4110e01975238d3747cbc49
Nicolas M <mbark.nicolas@gmail.com>
2025-02-11 15:36:54 +0100

    fix

--- config/session.js
+++ config/session.js
@@ -1,15 +1,15 @@
 const MongoStore = require("connect-mongo");
 
 module.exports.session = {
-  secret: process.env.SESSION_SECRET || "d54dcb5dc070ec612015b0e93ba026f9",
+  secret: "d54dcb5dc070ec612015b0e93ba026f9",
 
-  adapter: "connect-mongo",
+  adapter: "custom",
 
   store: MongoStore.create({
     mongoUrl: "mongodb://admin:pissenlit2025!@127.0.0.1:27017/sessions?authSource=admin",
     collectionName: "sessions",
     ttl: 14 * 24 * 60 * 60, // 14 jours d'expiration
-    autoRemove: "native", // Nettoyage automatique des sessions expirées
+    autoRemove: "native",
   }),
 
   cookie: {
